4dc49e053ea5a6677b51a5bcc78e34f1384e3ed4
m32 <m32@m32.io>
2025-03-07 23:35:05 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "tree-sitter grammars for arista style network configurations";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      perSystem = { inputs', self', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.tree-sitter
+            pkgs.nodejs
+            pkgs.node-gyp
+            pkgs.gcc
+          ];
+        };
+      };
+    };
+}
